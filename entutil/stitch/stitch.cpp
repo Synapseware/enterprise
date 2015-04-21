415202fdc042af68070fa3dbbe87cb635c821fac
Ramun McCallum <matthew@synapseware.com>
2015-04-20 21:37:26 -0700

    WIP - stitch file changes

--- entutil/stitch/stitch.cpp
+++ entutil/stitch/stitch.cpp
@@ -5,33 +5,60 @@ using namespace std;
 
 int main(int argc, char* argv[])
 {
-	cout << "argc: " << argc << "\n";
-
-	for (int i = 0; i < argc; i++)
-	{
-		cout << "arg " << i << " = '" << argv[i] << "'\n";
-	}
-
 	if (argc < 2)
 	{
 		showHelp();
 		return 0;
 	}
 
+	char* output = NULL;
+	char** files = (char**) malloc(argc * sizeof(char*));
+	int pageSize = 256;
+	int fileCount = 0;
 
-	// allocate the source file array
-	char* sourceFiles = new char[argc * 25];
-	const char* outputFile = "~/test.bin";
+	memset(files, 0, argc * sizeof(char*));
 
-	// allocate the stitch file class
+	for (int i = 1; i < argc; i++)
+	{
+		if (0 == strncmp("-o", argv[i], 2))
+			output = argv[++i];
+		else if (0 == strncmp("-p", argv[i], 2))
+			pageSize = atoi(argv[++i]);
+		else
+		{
+			files[fileCount++] = argv[i];
+		}
+	}
+
+	if (0 == fileCount)
+	{
+		cout << "Error: No input files specified." << endl;
+		return -1;
+	}
+
+	if (NULL == output)
+	{
+		cout << "Error: No output file specified." << endl;
+		return -1;
+	}
+
+	cout << "Stitching " << fileCount << " files together, into " << output << " with a pageSize of " << pageSize << endl;;
+
 	StitchFile * stitch = new StitchFile();
 
-	stitch->Process(outputFile, sourceFiles, argc - 2, 256);
+	bool error = false;
+	if (!stitch->Process(output, (const char**)files, fileCount, pageSize))
+	{
+		cout << "Failed to generate output file." << endl;
+		error = true;
+	}
 
 	delete(stitch);
-	delete(sourceFiles);
+	free(files);
 
-	return 0;
+	return error
+		? -1
+		: 0;
 }
 
 
@@ -40,9 +67,9 @@ void showHelp(void)
 {
 	cout << "stitch - wave utility builder for the Enterprise\n";
 	cout << "\n";
-	cout << "stitch -o outputfile -p {pagesize} {input file 1...n}\n";
+	cout << "stitch -o outputfile -p {pageSize} {input file 1...n}\n";
 	cout << "   combines a set of wav files into a single output file suitable for programming to the EEPROM on the Enterprise board.\n";
 	cout << "   stitch -o enterprise.bin -p 256 file1.wav file2.wav file3.wav\n";
-	cout << "   pagesize should be set to 256 for the AT24C series eeproms.\n";
+	cout << "   pageSize should be set to 256 for the AT24C series eeproms.\n";
 	cout << "\n\n";
 }
